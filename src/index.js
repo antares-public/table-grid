dd8f5922872c97c87b44137069e2e2b184731561
antaresofficial <antaresofficial@icloud.com>
2022-03-23 13:09:23 +0500

    rows valid red

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import { AgGridReact } from "ag-grid-react";
+import "./index.css";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 const App = () => {
   const [rowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 },
+    { make: "Toyota", model: "Celica", price: 35_000 },
+    { make: "Ford", model: "Mondeo", price: 32_000 },
+    { make: "Porsche", model: "Boxter", price: 72_000 },
   ]);
+
+  const rowClassRules = {
+    "row-fail": (params) => params.data.price > 50_000,
+    "row-pass": (params) => params.data.price <= 50_000,
+  };
 
   const [columnDefs] = useState([
     { field: "make" },
     { field: "model" },
     { field: "price" },
+    { field: "student", minWidth: 150 },
+    {
+      field: "result",
+      valueGetter: (params) => params.data.price,
+      cellRendererSelector: (params) => {
+        // console.log(params);
+        return null;
+      },
+    },
   ]);
 
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      <AgGridReact
+        rowClassRules={rowClassRules}
+        rowData={rowData}
+        columnDefs={columnDefs}
+      ></AgGridReact>
     </div>
   );
 };
